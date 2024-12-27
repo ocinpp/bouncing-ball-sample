9c0da614cbdbbeb3e410f197eeb28475db6e7cd2
ocinpp <ocin25@gmail.com>
2024-12-27 12:47:20 +0800

    Add new colours

--- src/colors.tsx
+++ src/colors.tsx
@@ -1,3 +1,11 @@
-// From `nice-color-palettes`, this one is index 17
-export default ['#99b898', '#fecea8', '#ff847c', '#e84a5f', '#2a363b'] as const
-// export default ['#000000', '#ffffff'] as const
+// Beautiful Blues
+// export default ['#011f4b' , '#03396c', '#005b96', '#6497b1', '#b3cde0'] as const
+
+// Never Dreamed Of This
+// export default ['#fff6e9', '#ffefd7', '#fffef9', '#e3f0ff', '#d2e7ff'] as const
+
+// The Armor Falls
+export default ['#bfd6f6', '#8dbdff', '#64a1f4', '#4a91f2', '#3b7dd8'] as const
+
+// Black and White
+// export default ['#000000', '#ffffff', '#000000', '#ffffff', '#000000'] as const
