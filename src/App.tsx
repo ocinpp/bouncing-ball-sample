8bfb8599acaf0d2c8d0695b5127df359282b8daf
ocinpp <ocin25@gmail.com>
2024-12-27 12:16:43 +0800

    Initial version

---
+++ src/App.tsx
@@ -0,0 +1,191 @@
+import type { PlaneProps, Triplet } from '@react-three/cannon'
+import { Physics, useBox, usePlane, useSphere, useContactMaterial } from '@react-three/cannon'
+import type { MeshPhongMaterialProps } from '@react-three/fiber'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { useMemo, useRef } from 'react'
+import type { InstancedMesh, Mesh } from 'three'
+import { Color } from 'three'
+
+import niceColors from './colors'
+
+type OurPlaneProps = Pick<MeshPhongMaterialProps, 'color'> & Pick<PlaneProps, 'position' | 'rotation'>
+
+const bouncyMaterial = {
+  name: 'bouncy',
+  /*
+  Restitution for this material.
+  If non-negative, it will be used instead of the restitution given by ContactMaterials.
+  If there's no matching ContactMaterial, the value from .defaultContactMaterial in the World will be used.
+  */
+  restitution: 1.1,
+}
+
+const boxMaterial = 'box'
+
+const groundMaterial = 'ground'
+
+/*
+Setting the friction on both materials prevents overriding the friction given by ContactMaterials.
+Since we want rubber to not be slippery we do not set this here and instead use a ContactMaterial.
+See https://github.com/pmndrs/cannon-es/blob/e9f1bccd8caa250cc6e6cdaf85389058e1c9238e/src/world/World.ts#L661-L673
+*/
+const rubberMaterial = 'rubber'
+
+const slipperyMaterial = {
+  /*
+  Friction for this material.
+  If non-negative, it will be used instead of the friction given by ContactMaterials.
+  If there's no matching ContactMaterial, the value from .defaultContactMaterial in the World will be used.
+  */
+  friction: 0,
+  name: 'slippery',
+}
+
+const useContactMaterials = (rubberSlips: boolean) => {
+  useContactMaterial(groundMaterial, groundMaterial, {
+    contactEquationRelaxation: 3,
+    contactEquationStiffness: 1e8,
+    friction: 0.4,
+    frictionEquationStiffness: 1e8,
+    restitution: 0.3,
+  })
+
+  useContactMaterial(boxMaterial, groundMaterial, {
+    contactEquationRelaxation: 3,
+    contactEquationStiffness: 1e8,
+    friction: 0.4,
+    frictionEquationStiffness: 1e8,
+    restitution: 0.3,
+  })
+  useContactMaterial(boxMaterial, slipperyMaterial, {
+    friction: 0,
+    restitution: 0.3,
+  })
+
+  useContactMaterial(groundMaterial, slipperyMaterial, {
+    friction: 0,
+    restitution: 0.3,
+  })
+  useContactMaterial(slipperyMaterial, slipperyMaterial, {
+    friction: 0.1,
+    restitution: 0.3,
+  })
+
+  useContactMaterial(bouncyMaterial, slipperyMaterial, {
+    friction: 0,
+    restitution: 0.5,
+  })
+  useContactMaterial(bouncyMaterial, groundMaterial, {
+    restitution: 0.8,
+  })
+  useContactMaterial(bouncyMaterial, bouncyMaterial, {
+    restitution: 0.8, // This does nothing because bouncyMaterial already has a restitution
+  })
+
+  useContactMaterial(
+    rubberMaterial,
+    slipperyMaterial,
+    {
+      friction: rubberSlips ? 0 : 1,
+      restitution: 0.3,
+    },
+    [rubberSlips],
+  )
+
+  useContactMaterial(rubberMaterial, bouncyMaterial, {
+    restitution: 0.5,
+  })
+}
+
+function Plane({ color, ...props }: OurPlaneProps) {
+  const [ref] = usePlane(() => ({ ...props, }), useRef<Mesh>(null))
+  return (
+    <mesh ref={ref} receiveShadow>
+      <planeGeometry args={[1000, 1000]} />
+      <meshPhongMaterial color={color} />
+    </mesh>
+  )
+}
+
+function Box() {
+  const boxSize: Triplet = [4, 4, 4]
+  const [ref, api] = useBox(() => ({ args: boxSize, mass: 1, type: 'Kinematic' }), useRef<Mesh>(null))
+  useFrame((state) => {
+    const t = state.clock.getElapsedTime()
+    api.position.set(Math.sin(t * 2) * 5, Math.cos(t * 2) * 5, 3)
+    api.rotation.set(Math.sin(t * 6), Math.cos(t * 6), 0)
+  })
+  return (
+    <mesh ref={ref} castShadow receiveShadow>
+      <boxGeometry args={boxSize} />
+      <meshLambertMaterial color="white" />
+    </mesh>
+  )
+}
+
+function InstancedSpheres({ number = 100 }) {
+  const [ref] = useSphere(
+    (index) => ({
+      args: [1],
+      mass: 1,
+      material: "bouncy",
+      position: [Math.random() - 0.5, Math.random() - 0.5, index * 2],
+    }),
+    useRef<InstancedMesh>(null),
+  )
+  const colors = useMemo(() => {
+    const array = new Float32Array(number * 3)
+    const color = new Color()
+    for (let i = 0; i < number; i++)
+      color
+        .set(niceColors[Math.floor(Math.random() * 5)])
+        .convertSRGBToLinear()
+        .toArray(array, i * 3)
+    return array
+  }, [number])
+
+  return (
+    <instancedMesh ref={ref} castShadow receiveShadow args={[undefined, undefined, number]}>
+      <sphereGeometry args={[1, 16, 16]}>
+        <instancedBufferAttribute attach="attributes-color" args={[colors, 3]} />
+      </sphereGeometry>
+      <meshPhongMaterial vertexColors />
+    </instancedMesh>
+  )
+}
+
+function PhysicsContent() {
+  useContactMaterials(false)
+
+  return (
+    <group>
+      <Plane color={niceColors[4]} material="ground" position={[0, 0, 0]} rotation={[0, 0, 0]} />
+      <Plane color={niceColors[1]} material="ground" position={[-10, 0, 0]} rotation={[0, 0.9, 0]} />
+      <Plane color={niceColors[2]} material="ground" position={[10, 0, 0]} rotation={[0, -0.9, 0]} />
+      <Plane color={niceColors[3]} material="ground" position={[0, 10, 0]} rotation={[0.9, 0, 0]} />
+      <Plane color={niceColors[0]} material="ground" position={[0, -10, 0]} rotation={[-0.9, 0, 0]} />
+      {/* <Box /> */}
+      <InstancedSpheres number={100} />
+    </group>
+  )
+}
+
+export default () => (
+  <Canvas camera={{ position: [0, 0, 25] }} shadows>
+    <hemisphereLight intensity={0.35 * Math.PI} />
+    <spotLight
+      angle={0.3}
+      castShadow
+      decay={0}
+      intensity={2 * Math.PI}
+      penumbra={1}
+      position={[30, 0, 30]}
+      shadow-mapSize-width={256}
+      shadow-mapSize-height={256}
+    />
+    <pointLight decay={0} intensity={0.5 * Math.PI} position={[-30, 0, -30]} />
+    <Physics gravity={[0, 0, -10]}>
+      <PhysicsContent />
+    </Physics>
+  </Canvas>
+)
---
+++ src/colors.tsx
@@ -0,0 +1,3 @@
+// From `nice-color-palettes`, this one is index 17
+export default ['#99b898', '#fecea8', '#ff847c', '#e84a5f', '#2a363b'] as const
+// export default ['#000000', '#ffffff'] as const
