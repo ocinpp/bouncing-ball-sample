0204d5581cf4439ae51681c6109f4b230a3ede4a
ocinpp <ocin25@gmail.com>
2024-12-27 12:51:37 +0800

    Add colours

--- src/colors.tsx
+++ src/colors.tsx
@@ -5,7 +5,13 @@
 // export default ['#fff6e9', '#ffefd7', '#fffef9', '#e3f0ff', '#d2e7ff'] as const
 
 // The Armor Falls
-export default ['#bfd6f6', '#8dbdff', '#64a1f4', '#4a91f2', '#3b7dd8'] as const
+// export default ['#bfd6f6', '#8dbdff', '#64a1f4', '#4a91f2', '#3b7dd8'] as const
 
 // Black and White
 // export default ['#000000', '#ffffff', '#000000', '#ffffff', '#000000'] as const
+
+// Moonlight Bytes 6
+// export default ['#4a4e4d', '#0e9aa7', '#3da4ab', '#f6cd61', '#fe8a71'] as const
+
+// Number 3
+export default ['#2a4d69', '#4b86b4', '#adcbe3', '#e7eff6', '#63ace5'] as const
